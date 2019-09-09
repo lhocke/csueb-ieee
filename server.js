3793833e2683a7da676f79d756cda90e1716c0cf
lhocke <dylan.ishihara@gmail.com>
2019-09-09 11:53:02 -0700

    updates index file for bootstrap

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const helmet = require ('helmet')
 const app = express();
+const path = require('path')
 const PORT = process.env.PORT || 3000;
 
 app.use(helmet())
 
-app.use(express.static('public'))
+app.use(express.static(path.resolve(__dirname,'public')))
+app.use(express.static(path.resolve(__dirname,'views')))
+
+// var index = require('./views/index.html')
+
+// app.get('/', (req,res)=>{
+//     res.send
+// })
 
 app.listen(PORT, () => {                // ExpressJS server start
     console.log(`Listening on ${PORT}`) // Output port number
