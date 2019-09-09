acf20163e30afdbbf545d97fa3c4f303c696fd80
lhocke <dylan.ishihara@gmail.com>
2019-09-09 11:13:58 -0700

    adds helmet

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const helmet = require ('helmet')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+app.use(helmet())
 
 app.listen(PORT, () => {                // ExpressJS server start
     console.log(`Listening on ${PORT}`) // Output port number
