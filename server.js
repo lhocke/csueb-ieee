ec5d7f159e8ac7e9e713b629f2eef4ddc536ee6e
Dylan Ishihara <lhocke@Dylans-MacBook-Air.local>
2019-09-09 15:55:53 -0700

    fixes default routing issue

--- server.js
+++ server.js
@@ -9,11 +9,9 @@ app.use(helmet())
 app.use(express.static(path.resolve(__dirname,'public')))
 app.use(express.static(path.resolve(__dirname,'views')))
 
-// var index = require('./views/index.html')
+var indexRouter = require('./routes/index')
 
-// app.get('/', (req,res)=>{
-//     res.send
-// })
+app.use('/', indexRouter)
 
 app.listen(PORT, () => {                // ExpressJS server start
     console.log(`Listening on ${PORT}`) // Output port number
